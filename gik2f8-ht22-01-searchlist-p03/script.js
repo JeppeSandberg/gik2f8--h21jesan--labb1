87ba0344e8977961ad01da95b40a9b4eb6a9ad4b
h21jesan <h21jeasn@du.se>
2022-12-21 09:55:06 +0100

    nytt utseende

--- gik2f8-ht22-01-searchlist-p03/script.js
+++ gik2f8-ht22-01-searchlist-p03/script.js
@@ -1,32 +1,55 @@
 'use strict';
-
 let bookList = [];
-
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   getAll().then((apiBooks) => (bookList = apiBooks));
 });
-
-searchField.addEventListener('keyup', (e) =>
+searchField.addEventListener('keyup', (e) => searchBooks(e.target.value));
+async function searchBooks(searchTerm) {
   renderBookList(
-    bookList.filter(({ title, author }) => {
-      const searchTerm = e.target.value.toLowerCase();
-      return (
-        title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        author.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-    })
-  )
-);
-
-function moreinfo(x) {
-  x.height = "32px";
-}
+    bookList.filter(
+      ({ title, author }) =>
+        title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ||
+        author.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0
+    )
+  );
+};
 
 function renderBookList(bookList) {
-  const existingElement = document.querySelector('.book-list');
-
+  BookList(bookList);
+  const existingElementBookList = document.querySelector('.book-list');
   const root = document.getElementById('root');
-
-  existingElement && root.removeChild(existingElement);
+  existingElementBookList && root.removeChild(existingElementBookList);
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+
+  const bookListHover = document.querySelectorAll('.book-list__item');
+  bookListHover.forEach((item) => {
+    console.log(item.innerHTML);
+    var innerHTML;
+    innerHTML = item.innerHTML;
+    item.addEventListener('mouseover', () => renderBookListPlusFetch(innerHTML));
+    item.addEventListener('mouseout', () => {
+      const deleteElement = document.getElementById("bookDetail");
+      deleteElement && deleteElement.remove();
+    });
+  });
 }
+
+async function getOneBook(id) {
+  const result = await fetch(url + "/" + id).then((result) => result.json());
+  return result;
+}
+
+async function renderBookListPlusFetch(innerHTML) {
+  for (let i = 1; i <= bookList.length; i++) {
+    const book = await getOneBook(i);
+    if (innerHTML.toLowerCase().indexOf(book.author.toLowerCase()) >= 0) {
+      let html;
+      html = BookInfo(book);
+      const bookInfoPlacement = document.querySelector('.book-list');
+      bookInfoPlacement.insertAdjacentHTML("afterbegin", html);
+    }
+  }
+}
+
+
+
